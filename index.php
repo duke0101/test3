f0d3848ceea85775802fa5a48cdc262305256e0f
duke0101 <39301717+duke0101@users.noreply.github.com>
2018-05-15 13:22:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
  <body>
  <?php 
 echo '<h3> my PHP page </h3>';
-echo '<a href="http://www.facebook.com">facebook</a>\n';
+echo '<a href="http://www.facebook.com">facebook</a>';
+echo '\n';
 echo '<a href="http://www.yahoo.com">News</a>';
 echo '<h1><p>oh yeah</p></h1>'; 
 ?> 
