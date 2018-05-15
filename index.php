4928965bb70b88f64bb1be22003c4844d766918c
duke0101 <39301717+duke0101@users.noreply.github.com>
2018-05-15 12:54:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<h1><p>oh yeah</p></h1>'; ?> 
+ <?php 
+echo '<h3> my PHP page </h3>';
+echo '<h1><p>oh yeah</p></h1>'; 
+?> 
  </body>
 </html>
